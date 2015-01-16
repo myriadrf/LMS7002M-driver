123bab8aad47a5ccd0b762a1593794fe050a0ae8
Josh Blum <josh@joshknows.com>
2015-01-15 18:51:48 -0800

    rx and tx tsp registers

--- include/LMS7002M/LMS7002M_regs.h
+++ include/LMS7002M/LMS7002M_regs.h
@@ -121,8 +121,43 @@ static inline int LMS7002M_regs_get(LMS7002M_regs_t *regs, const int addr);
 #define REG_0X0089_TST_CGEN_VCO_TUNE 3
 #define REG_0X0089_TST_CGEN_VCO_TUNE_50_KOHM 2
 #define REG_0X0089_TST_CGEN_TSTDO 1
+#define REG_0X0200_TSGFC_NEG6DB 0
+#define REG_0X0200_TSGFC_FS 1
+#define REG_0X0200_TSGFCW_DIV4 2
+#define REG_0X0200_TSGFCW_DIV8 1
+#define REG_0X0200_TSGMODE_DC 1
+#define REG_0X0200_TSGMODE_NCO 0
+#define REG_0X0200_INSEL_TEST 1
+#define REG_0X0200_INSEL_LML 0
+#define REG_0X0203_HBI_OVR_BYPASS 7
+#define REG_0X0208_CMIX_GAIN_NEG6DB 2
+#define REG_0X0208_CMIX_GAIN_POS6DB 1
+#define REG_0X0208_CMIX_GAIN_0DB 0
+#define REG_0X0208_CMIX_SC_UPCONVERT 0
+#define REG_0X0208_CMIX_SC_DOWNCONVERT 1
 #define REG_0X0240_MODE_PHO 1
 #define REG_0X0240_MODE_FCW 0
+#define REG_0X0400_CAPSEL_BSIGQ_BSTATE 3
+#define REG_0X0400_CAPSEL_RSSI 0
+#define REG_0X0400_CAPSEL_BSIGI_BSTATE 2
+#define REG_0X0400_CAPSEL_ADCI_ADCQ 1
+#define REG_0X0400_TSGFC_NEG6DB 0
+#define REG_0X0400_TSGFC_FS 1
+#define REG_0X0400_TSGFCW_DIV4 2
+#define REG_0X0400_TSGFCW_DIV8 1
+#define REG_0X0400_TSGMODE_DC 1
+#define REG_0X0400_TSGMODE_NCO 0
+#define REG_0X0400_INSEL_TEST 1
+#define REG_0X0400_INSEL_LML 0
+#define REG_0X0403_HBI_OVR_BYPASS 7
+#define REG_0X040A_AGC_MODE_RSSI 1
+#define REG_0X040A_AGC_MODE_AGC 0
+#define REG_0X040A_AGC_MODE_BYPASS 2
+#define REG_0X040C_CMIX_GAIN_NEG6DB 2
+#define REG_0X040C_CMIX_GAIN_POS6DB 1
+#define REG_0X040C_CMIX_GAIN_0DB 0
+#define REG_0X040C_CMIX_SC_UPCONVERT 0
+#define REG_0X040C_CMIX_SC_DOWNCONVERT 1
 
 struct LMS7002M_regs_struct
 {
@@ -250,12 +285,81 @@ struct LMS7002M_regs_struct
     int reg_0x008c_cp3_cgen;
     int reg_0x008c_cz_cgen;
     int reg_0x008d_resrv_cgn;
+    int reg_0x0200_tsgfc;
+    int reg_0x0200_tsgfcw;
+    int reg_0x0200_tsgdcldq;
+    int reg_0x0200_tsgdcldi;
+    int reg_0x0200_tsgswapiq;
+    int reg_0x0200_tsgmode;
+    int reg_0x0200_insel;
+    int reg_0x0200_bstart;
+    int reg_0x0200_en;
+    int reg_0x0201_gcorrq;
+    int reg_0x0202_gcorri;
+    int reg_0x0203_hbi_ovr;
+    int reg_0x0203_iqcorr;
+    int reg_0x0204_dccorri;
+    int reg_0x0204_dccorrq;
+    int reg_0x0205_gfir1_l;
+    int reg_0x0205_gfir1_n;
+    int reg_0x0206_gfir2_l;
+    int reg_0x0206_gfir2_n;
+    int reg_0x0207_gfir3_l;
+    int reg_0x0207_gfir3_n;
+    int reg_0x0208_cmix_gain;
+    int reg_0x0208_cmix_sc;
+    int reg_0x0208_cmix_byp;
+    int reg_0x0208_isinc_byp;
+    int reg_0x0208_gfir3_byp;
+    int reg_0x0208_gfir2_byp;
+    int reg_0x0208_gfir1_byp;
+    int reg_0x0208_dc_byp;
+    int reg_0x0208_gc_byp;
+    int reg_0x0208_ph_byp;
     int reg_0x0240_dthbit;
     int reg_0x0240_sel;
     int reg_0x0240_mode;
     int reg_0x0241_pho;
     int reg_0x0242_fcw0_hi;
     int reg_0x0243_fcw0_lo;
+    int reg_0x0400_capsel;
+    int reg_0x0400_tsgfc;
+    int reg_0x0400_capture;
+    int reg_0x0400_tsgfcw;
+    int reg_0x0400_tsgdcldq;
+    int reg_0x0400_tsgdcldi;
+    int reg_0x0400_tsgswapiq;
+    int reg_0x0400_tsgmode;
+    int reg_0x0400_insel;
+    int reg_0x0400_bstart;
+    int reg_0x0400_en;
+    int reg_0x0401_gcorrq;
+    int reg_0x0402_gcorri;
+    int reg_0x0403_hbi_ovr;
+    int reg_0x0403_iqcorr;
+    int reg_0x0404_dccorr_avg;
+    int reg_0x0405_gfir1_l;
+    int reg_0x0405_gfir1_n;
+    int reg_0x0406_gfir2_l;
+    int reg_0x0406_gfir2_n;
+    int reg_0x0407_gfir3_l;
+    int reg_0x0407_gfir3_n;
+    int reg_0x0408_agc_k_lsb;
+    int reg_0x0409_agc_adesired;
+    int reg_0x0409_agc_k_msb;
+    int reg_0x040a_agc_mode;
+    int reg_0x040a_agc_avg;
+    int reg_0x040b_dc_reg;
+    int reg_0x040c_cmix_gain;
+    int reg_0x040c_cmix_sc;
+    int reg_0x040c_cmix_byp;
+    int reg_0x040c_isinc_byp;
+    int reg_0x040c_gfir3_byp;
+    int reg_0x040c_gfir2_byp;
+    int reg_0x040c_gfir1_byp;
+    int reg_0x040c_dc_byp;
+    int reg_0x040c_gc_byp;
+    int reg_0x040c_ph_byp;
 };
 
 /***********************************************************************
@@ -282,10 +386,32 @@ static inline void LMS7002M_regs_init(LMS7002M_regs_t *regs)
     LMS7002M_regs_set(regs, 0x008B, 0x1900);
     LMS7002M_regs_set(regs, 0x008C, 0x67b);
     LMS7002M_regs_set(regs, 0x008D, 0x0);
+    LMS7002M_regs_set(regs, 0x0200, 0x81);
+    LMS7002M_regs_set(regs, 0x0201, 0x7ff);
+    LMS7002M_regs_set(regs, 0x0202, 0x7ff);
+    LMS7002M_regs_set(regs, 0x0203, 0x0);
+    LMS7002M_regs_set(regs, 0x0204, 0x0);
+    LMS7002M_regs_set(regs, 0x0205, 0x0);
+    LMS7002M_regs_set(regs, 0x0206, 0x0);
+    LMS7002M_regs_set(regs, 0x0207, 0x0);
+    LMS7002M_regs_set(regs, 0x0208, 0x0);
     LMS7002M_regs_set(regs, 0x0240, 0x20);
     LMS7002M_regs_set(regs, 0x0241, 0x0);
     LMS7002M_regs_set(regs, 0x0242, 0x0);
     LMS7002M_regs_set(regs, 0x0243, 0x0);
+    LMS7002M_regs_set(regs, 0x0400, 0x81);
+    LMS7002M_regs_set(regs, 0x0401, 0x7ff);
+    LMS7002M_regs_set(regs, 0x0402, 0x7ff);
+    LMS7002M_regs_set(regs, 0x0403, 0x0);
+    LMS7002M_regs_set(regs, 0x0404, 0x0);
+    LMS7002M_regs_set(regs, 0x0405, 0x0);
+    LMS7002M_regs_set(regs, 0x0406, 0x0);
+    LMS7002M_regs_set(regs, 0x0407, 0x0);
+    LMS7002M_regs_set(regs, 0x0408, 0x0);
+    LMS7002M_regs_set(regs, 0x0409, 0x0);
+    LMS7002M_regs_set(regs, 0x040A, 0x0);
+    LMS7002M_regs_set(regs, 0x040B, 0x0);
+    LMS7002M_regs_set(regs, 0x040C, 0x0);
 }
 
 static inline void LMS7002M_regs_set(LMS7002M_regs_t *regs, const int addr, const int value)
@@ -486,6 +612,73 @@ static inline void LMS7002M_regs_set(LMS7002M_regs_t *regs, const int addr, cons
         regs->reg_0x008d_resrv_cgn = (value >> 0) & 0x7;
         return;
     }
+    if (addr == 0x0200)
+    {
+        regs->reg_0x0200_tsgfc = (value >> 9) & 0x1;
+        regs->reg_0x0200_tsgfcw = (value >> 7) & 0x3;
+        regs->reg_0x0200_tsgdcldq = (value >> 6) & 0x1;
+        regs->reg_0x0200_tsgdcldi = (value >> 5) & 0x1;
+        regs->reg_0x0200_tsgswapiq = (value >> 4) & 0x1;
+        regs->reg_0x0200_tsgmode = (value >> 3) & 0x1;
+        regs->reg_0x0200_insel = (value >> 2) & 0x1;
+        regs->reg_0x0200_bstart = (value >> 1) & 0x1;
+        regs->reg_0x0200_en = (value >> 0) & 0x1;
+        return;
+    }
+    if (addr == 0x0201)
+    {
+        regs->reg_0x0201_gcorrq = (value >> 0) & 0x7ff;
+        return;
+    }
+    if (addr == 0x0202)
+    {
+        regs->reg_0x0202_gcorri = (value >> 0) & 0x7ff;
+        return;
+    }
+    if (addr == 0x0203)
+    {
+        regs->reg_0x0203_hbi_ovr = (value >> 12) & 0x7;
+        regs->reg_0x0203_iqcorr = (value >> 0) & 0xfff;
+        return;
+    }
+    if (addr == 0x0204)
+    {
+        regs->reg_0x0204_dccorri = (value >> 8) & 0xff;
+        regs->reg_0x0204_dccorrq = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0205)
+    {
+        regs->reg_0x0205_gfir1_l = (value >> 8) & 0x7;
+        regs->reg_0x0205_gfir1_n = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0206)
+    {
+        regs->reg_0x0206_gfir2_l = (value >> 8) & 0x7;
+        regs->reg_0x0206_gfir2_n = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0207)
+    {
+        regs->reg_0x0207_gfir3_l = (value >> 8) & 0x7;
+        regs->reg_0x0207_gfir3_n = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0208)
+    {
+        regs->reg_0x0208_cmix_gain = (value >> 14) & 0x3;
+        regs->reg_0x0208_cmix_sc = (value >> 13) & 0x1;
+        regs->reg_0x0208_cmix_byp = (value >> 8) & 0x1;
+        regs->reg_0x0208_isinc_byp = (value >> 7) & 0x1;
+        regs->reg_0x0208_gfir3_byp = (value >> 6) & 0x1;
+        regs->reg_0x0208_gfir2_byp = (value >> 5) & 0x1;
+        regs->reg_0x0208_gfir1_byp = (value >> 4) & 0x1;
+        regs->reg_0x0208_dc_byp = (value >> 3) & 0x1;
+        regs->reg_0x0208_gc_byp = (value >> 1) & 0x1;
+        regs->reg_0x0208_ph_byp = (value >> 0) & 0x1;
+        return;
+    }
     if (addr == 0x0240)
     {
         regs->reg_0x0240_dthbit = (value >> 5) & 0xf;
@@ -506,6 +699,96 @@ static inline void LMS7002M_regs_set(LMS7002M_regs_t *regs, const int addr, cons
     if (addr == 0x0243)
     {
         regs->reg_0x0243_fcw0_lo = (value >> 0) & 0xffff;
+        return;
+    }
+    if (addr == 0x0400)
+    {
+        regs->reg_0x0400_capsel = (value >> 13) & 0x3;
+        regs->reg_0x0400_tsgfc = (value >> 9) & 0x1;
+        regs->reg_0x0400_capture = (value >> 9) & 0x1;
+        regs->reg_0x0400_tsgfcw = (value >> 7) & 0x3;
+        regs->reg_0x0400_tsgdcldq = (value >> 6) & 0x1;
+        regs->reg_0x0400_tsgdcldi = (value >> 5) & 0x1;
+        regs->reg_0x0400_tsgswapiq = (value >> 4) & 0x1;
+        regs->reg_0x0400_tsgmode = (value >> 3) & 0x1;
+        regs->reg_0x0400_insel = (value >> 2) & 0x1;
+        regs->reg_0x0400_bstart = (value >> 1) & 0x1;
+        regs->reg_0x0400_en = (value >> 0) & 0x1;
+        return;
+    }
+    if (addr == 0x0401)
+    {
+        regs->reg_0x0401_gcorrq = (value >> 0) & 0x7ff;
+        return;
+    }
+    if (addr == 0x0402)
+    {
+        regs->reg_0x0402_gcorri = (value >> 0) & 0x7ff;
+        return;
+    }
+    if (addr == 0x0403)
+    {
+        regs->reg_0x0403_hbi_ovr = (value >> 12) & 0x7;
+        regs->reg_0x0403_iqcorr = (value >> 0) & 0xfff;
+        return;
+    }
+    if (addr == 0x0404)
+    {
+        regs->reg_0x0404_dccorr_avg = (value >> 0) & 0x7;
+        return;
+    }
+    if (addr == 0x0405)
+    {
+        regs->reg_0x0405_gfir1_l = (value >> 8) & 0x7;
+        regs->reg_0x0405_gfir1_n = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0406)
+    {
+        regs->reg_0x0406_gfir2_l = (value >> 8) & 0x7;
+        regs->reg_0x0406_gfir2_n = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0407)
+    {
+        regs->reg_0x0407_gfir3_l = (value >> 8) & 0x7;
+        regs->reg_0x0407_gfir3_n = (value >> 0) & 0xff;
+        return;
+    }
+    if (addr == 0x0408)
+    {
+        regs->reg_0x0408_agc_k_lsb = (value >> 0) & 0xffff;
+        return;
+    }
+    if (addr == 0x0409)
+    {
+        regs->reg_0x0409_agc_adesired = (value >> 4) & 0xfff;
+        regs->reg_0x0409_agc_k_msb = (value >> 0) & 0x3;
+        return;
+    }
+    if (addr == 0x040A)
+    {
+        regs->reg_0x040a_agc_mode = (value >> 12) & 0x3;
+        regs->reg_0x040a_agc_avg = (value >> 0) & 0x7;
+        return;
+    }
+    if (addr == 0x040B)
+    {
+        regs->reg_0x040b_dc_reg = (value >> 0) & 0xffff;
+        return;
+    }
+    if (addr == 0x040C)
+    {
+        regs->reg_0x040c_cmix_gain = (value >> 14) & 0x3;
+        regs->reg_0x040c_cmix_sc = (value >> 13) & 0x1;
+        regs->reg_0x040c_cmix_byp = (value >> 8) & 0x1;
+        regs->reg_0x040c_isinc_byp = (value >> 7) & 0x1;
+        regs->reg_0x040c_gfir3_byp = (value >> 6) & 0x1;
+        regs->reg_0x040c_gfir2_byp = (value >> 5) & 0x1;
+        regs->reg_0x040c_gfir1_byp = (value >> 4) & 0x1;
+        regs->reg_0x040c_dc_byp = (value >> 3) & 0x1;
+        regs->reg_0x040c_gc_byp = (value >> 1) & 0x1;
+        regs->reg_0x040c_ph_byp = (value >> 0) & 0x1;
         return;
     }
 }
@@ -691,6 +974,64 @@ static inline int LMS7002M_regs_get(LMS7002M_regs_t *regs, const int addr)
     {
         value |= (regs->reg_0x008d_resrv_cgn & 0x7) << 0;
     }
+    if (addr == 0x0200)
+    {
+        value |= (regs->reg_0x0200_tsgfc & 0x1) << 9;
+        value |= (regs->reg_0x0200_tsgfcw & 0x3) << 7;
+        value |= (regs->reg_0x0200_tsgdcldq & 0x1) << 6;
+        value |= (regs->reg_0x0200_tsgdcldi & 0x1) << 5;
+        value |= (regs->reg_0x0200_tsgswapiq & 0x1) << 4;
+        value |= (regs->reg_0x0200_tsgmode & 0x1) << 3;
+        value |= (regs->reg_0x0200_insel & 0x1) << 2;
+        value |= (regs->reg_0x0200_bstart & 0x1) << 1;
+        value |= (regs->reg_0x0200_en & 0x1) << 0;
+    }
+    if (addr == 0x0201)
+    {
+        value |= (regs->reg_0x0201_gcorrq & 0x7ff) << 0;
+    }
+    if (addr == 0x0202)
+    {
+        value |= (regs->reg_0x0202_gcorri & 0x7ff) << 0;
+    }
+    if (addr == 0x0203)
+    {
+        value |= (regs->reg_0x0203_hbi_ovr & 0x7) << 12;
+        value |= (regs->reg_0x0203_iqcorr & 0xfff) << 0;
+    }
+    if (addr == 0x0204)
+    {
+        value |= (regs->reg_0x0204_dccorri & 0xff) << 8;
+        value |= (regs->reg_0x0204_dccorrq & 0xff) << 0;
+    }
+    if (addr == 0x0205)
+    {
+        value |= (regs->reg_0x0205_gfir1_l & 0x7) << 8;
+        value |= (regs->reg_0x0205_gfir1_n & 0xff) << 0;
+    }
+    if (addr == 0x0206)
+    {
+        value |= (regs->reg_0x0206_gfir2_l & 0x7) << 8;
+        value |= (regs->reg_0x0206_gfir2_n & 0xff) << 0;
+    }
+    if (addr == 0x0207)
+    {
+        value |= (regs->reg_0x0207_gfir3_l & 0x7) << 8;
+        value |= (regs->reg_0x0207_gfir3_n & 0xff) << 0;
+    }
+    if (addr == 0x0208)
+    {
+        value |= (regs->reg_0x0208_cmix_gain & 0x3) << 14;
+        value |= (regs->reg_0x0208_cmix_sc & 0x1) << 13;
+        value |= (regs->reg_0x0208_cmix_byp & 0x1) << 8;
+        value |= (regs->reg_0x0208_isinc_byp & 0x1) << 7;
+        value |= (regs->reg_0x0208_gfir3_byp & 0x1) << 6;
+        value |= (regs->reg_0x0208_gfir2_byp & 0x1) << 5;
+        value |= (regs->reg_0x0208_gfir1_byp & 0x1) << 4;
+        value |= (regs->reg_0x0208_dc_byp & 0x1) << 3;
+        value |= (regs->reg_0x0208_gc_byp & 0x1) << 1;
+        value |= (regs->reg_0x0208_ph_byp & 0x1) << 0;
+    }
     if (addr == 0x0240)
     {
         value |= (regs->reg_0x0240_dthbit & 0xf) << 5;
@@ -708,6 +1049,83 @@ static inline int LMS7002M_regs_get(LMS7002M_regs_t *regs, const int addr)
     if (addr == 0x0243)
     {
         value |= (regs->reg_0x0243_fcw0_lo & 0xffff) << 0;
+    }
+    if (addr == 0x0400)
+    {
+        value |= (regs->reg_0x0400_capsel & 0x3) << 13;
+        value |= (regs->reg_0x0400_tsgfc & 0x1) << 9;
+        value |= (regs->reg_0x0400_capture & 0x1) << 9;
+        value |= (regs->reg_0x0400_tsgfcw & 0x3) << 7;
+        value |= (regs->reg_0x0400_tsgdcldq & 0x1) << 6;
+        value |= (regs->reg_0x0400_tsgdcldi & 0x1) << 5;
+        value |= (regs->reg_0x0400_tsgswapiq & 0x1) << 4;
+        value |= (regs->reg_0x0400_tsgmode & 0x1) << 3;
+        value |= (regs->reg_0x0400_insel & 0x1) << 2;
+        value |= (regs->reg_0x0400_bstart & 0x1) << 1;
+        value |= (regs->reg_0x0400_en & 0x1) << 0;
+    }
+    if (addr == 0x0401)
+    {
+        value |= (regs->reg_0x0401_gcorrq & 0x7ff) << 0;
+    }
+    if (addr == 0x0402)
+    {
+        value |= (regs->reg_0x0402_gcorri & 0x7ff) << 0;
+    }
+    if (addr == 0x0403)
+    {
+        value |= (regs->reg_0x0403_hbi_ovr & 0x7) << 12;
+        value |= (regs->reg_0x0403_iqcorr & 0xfff) << 0;
+    }
+    if (addr == 0x0404)
+    {
+        value |= (regs->reg_0x0404_dccorr_avg & 0x7) << 0;
+    }
+    if (addr == 0x0405)
+    {
+        value |= (regs->reg_0x0405_gfir1_l & 0x7) << 8;
+        value |= (regs->reg_0x0405_gfir1_n & 0xff) << 0;
+    }
+    if (addr == 0x0406)
+    {
+        value |= (regs->reg_0x0406_gfir2_l & 0x7) << 8;
+        value |= (regs->reg_0x0406_gfir2_n & 0xff) << 0;
+    }
+    if (addr == 0x0407)
+    {
+        value |= (regs->reg_0x0407_gfir3_l & 0x7) << 8;
+        value |= (regs->reg_0x0407_gfir3_n & 0xff) << 0;
+    }
+    if (addr == 0x0408)
+    {
+        value |= (regs->reg_0x0408_agc_k_lsb & 0xffff) << 0;
+    }
+    if (addr == 0x0409)
+    {
+        value |= (regs->reg_0x0409_agc_adesired & 0xfff) << 4;
+        value |= (regs->reg_0x0409_agc_k_msb & 0x3) << 0;
+    }
+    if (addr == 0x040A)
+    {
+        value |= (regs->reg_0x040a_agc_mode & 0x3) << 12;
+        value |= (regs->reg_0x040a_agc_avg & 0x7) << 0;
+    }
+    if (addr == 0x040B)
+    {
+        value |= (regs->reg_0x040b_dc_reg & 0xffff) << 0;
+    }
+    if (addr == 0x040C)
+    {
+        value |= (regs->reg_0x040c_cmix_gain & 0x3) << 14;
+        value |= (regs->reg_0x040c_cmix_sc & 0x1) << 13;
+        value |= (regs->reg_0x040c_cmix_byp & 0x1) << 8;
+        value |= (regs->reg_0x040c_isinc_byp & 0x1) << 7;
+        value |= (regs->reg_0x040c_gfir3_byp & 0x1) << 6;
+        value |= (regs->reg_0x040c_gfir2_byp & 0x1) << 5;
+        value |= (regs->reg_0x040c_gfir1_byp & 0x1) << 4;
+        value |= (regs->reg_0x040c_dc_byp & 0x1) << 3;
+        value |= (regs->reg_0x040c_gc_byp & 0x1) << 1;
+        value |= (regs->reg_0x040c_ph_byp & 0x1) << 0;
     }
     return value;
 }
