bbaeb20ec04cb0335175c38e74d00870cc639af5
Josh Blum <josh@joshknows.com>
2017-01-10 18:46:09 -0800

    Implement VCO CSW split search
    This is the split search implementation as similar to limesuite.
    The entire search range is split into two and swept separately.
    The overall ranges are combined based on the results.

--- src/LMS7002M_vco.c
+++ src/LMS7002M_vco.c
@@ -28,18 +28,24 @@ static void LMS7002M_read_vco_cmp(LMS7002M_t *self, const int vco_cmp_addr)
     LMS7002M_regs_spi_read(self, vco_cmp_addr);
 }
 
-int LMS7002M_tune_vco(
+/*!
+ * Sweep though the 7 lower CSW positions given a starting point.
+ * This function is invoked twice to sweep upper and lower ranges.
+ */
+static int LMS7002M_tune_vco_sweep(
     LMS7002M_t *self,
     int *vco_csw_reg,
     const int vco_csw_addr,
     int *vco_cmpho_reg,
     int *vco_cmplo_reg,
-    const int vco_cmp_addr
+    const int vco_cmp_addr,
+    const int start_point,
+    int *lo, int *hi
 )
 {
-    int csw_lowest = -1;
-    *vco_csw_reg = 0;
-    for (int i = 7; i >= 0; i--)
+    int csw_lowest = start_point+128;
+    *vco_csw_reg = start_point;
+    for (int i = 6; i >= 0; i--)
     {
         *vco_csw_reg |= 1 << i;
         LMS7002M_regs_spi_write(self, vco_csw_addr);
@@ -50,7 +56,7 @@ int LMS7002M_tune_vco(
         {
             *vco_csw_reg &= ~(1 << i); //clear bit i
         }
-        if (*vco_cmpho_reg != 0 && *vco_cmplo_reg == 0 && csw_lowest < 0)
+        if (*vco_cmpho_reg != 0 && *vco_cmplo_reg == 0 && *vco_csw_reg < csw_lowest)
         {
             csw_lowest = *vco_csw_reg;
         }
@@ -58,28 +64,83 @@ int LMS7002M_tune_vco(
     }
 
     //find the midpoint for the high and low bounds
-    if (csw_lowest >= 0)
+    int csw_highest = *vco_csw_reg;
+    while (csw_lowest <= csw_highest && csw_lowest > start_point)
     {
-        int csw_highest = *vco_csw_reg;
-        if (csw_lowest == csw_highest)
-        {
-            while (csw_lowest >= 0)
-            {
-                *vco_csw_reg = csw_lowest;
-                LMS7002M_regs_spi_write(self, vco_csw_addr);
-                LMS7002M_read_vco_cmp(self, vco_cmp_addr);
-
-                if (*vco_cmpho_reg == 0 && *vco_cmplo_reg == 0) break;
-                else csw_lowest--;
-            }
-            if (csw_lowest < 0) csw_lowest = 0;
-        }
-        csw_lowest += 1;
-
-        LMS7_logf(LMS7_INFO, "lowest CSW_VCO %i, highest CSW_VCO %i", csw_lowest, csw_highest);
-        *vco_csw_reg = (csw_highest+csw_lowest)/2;
+        csw_lowest--;
+        *vco_csw_reg = csw_lowest;
         LMS7002M_regs_spi_write(self, vco_csw_addr);
+        LMS7002M_read_vco_cmp(self, vco_cmp_addr);
+
+        if (*vco_cmpho_reg != 0 && *vco_cmplo_reg == 0) continue;
+        csw_lowest++;
+        break;
     }
+
+    *lo = csw_lowest;
+    *hi = csw_highest;
+    LMS7_logf(LMS7_INFO, "lowest CSW_VCO %i, highest CSW_VCO %i", csw_lowest, csw_highest);
+    return 0;
+}
+
+int LMS7002M_tune_vco(
+    LMS7002M_t *self,
+    int *vco_csw_reg,
+    const int vco_csw_addr,
+    int *vco_cmpho_reg,
+    int *vco_cmplo_reg,
+    const int vco_cmp_addr
+)
+{
+    //check comparator under minimum setting
+    *vco_csw_reg = 0;
+    LMS7002M_regs_spi_write(self, vco_csw_addr);
+    LMS7002M_read_vco_cmp(self, vco_cmp_addr);
+    if (*vco_cmpho_reg == 1 && *vco_cmplo_reg == 1)
+    {
+        LMS7_log(LMS7_ERROR, "VCO select FAIL - too high");
+        return -1;
+    }
+
+    //check comparator under maximum setting
+    *vco_csw_reg = 255;
+    LMS7002M_regs_spi_write(self, vco_csw_addr);
+    LMS7002M_read_vco_cmp(self, vco_cmp_addr);
+    if (*vco_cmpho_reg == 0 && *vco_cmplo_reg == 0)
+    {
+        LMS7_log(LMS7_ERROR, "VCO select FAIL - too low");
+        return -1;
+    }
+
+    //search both segments of the 8-bit space
+    int lo0, hi0, lo1, hi1;
+    LMS7002M_tune_vco_sweep(self, vco_csw_reg, vco_csw_addr, vco_cmpho_reg, vco_cmplo_reg, vco_cmp_addr, 0, &lo0, &hi0);
+    LMS7002M_tune_vco_sweep(self, vco_csw_reg, vco_csw_addr, vco_cmpho_reg, vco_cmplo_reg, vco_cmp_addr, 128, &lo1, &hi1);
+
+    //determine overall high-low with overlap
+    int csw_lowest, csw_highest;
+    if (hi0 == lo1-1)
+    {
+        csw_lowest = lo0;
+        csw_highest = hi1;
+    }
+
+    //otherwise use bigger range
+    else if ((hi0-lo0) > (hi1-lo1))
+    {
+        csw_lowest = lo0;
+        csw_highest = hi0;
+    }
+    else
+    {
+        csw_lowest = lo1;
+        csw_highest = hi1;
+    }
+
+    //set the midpoint of the search
+    *vco_csw_reg = (csw_highest+csw_lowest)/2;
+    LMS7002M_regs_spi_write(self, vco_csw_addr);
+    LMS7_logf(LMS7_INFO, "lowest CSW_VCO %i, highest CSW_VCO %i, CSW_VCO %i", csw_lowest, csw_highest, *vco_csw_reg);
 
     //check that the vco selection was successful
     LMS7002M_read_vco_cmp(self, vco_cmp_addr);
