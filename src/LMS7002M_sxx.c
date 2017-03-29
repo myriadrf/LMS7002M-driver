6c675baa2c24a1bab7c45078fe4cd60e07796336
Josh Blum <josh@joshknows.com>
2017-03-29 13:42:08 -0700

    LO tune sweeps over each VCO option
    Different from the last iteration,
    now we check each VCO for individual
    configurations with different fdivs

--- src/LMS7002M_sxx.c
+++ src/LMS7002M_sxx.c
@@ -27,64 +27,75 @@ void LMS7002M_sxx_enable(LMS7002M_t *self, const LMS7002M_dir_t direction, const
     LMS7002M_regs_spi_write(self, 0x011c);
 }
 
-int LMS7002M_set_lo_freq(LMS7002M_t *self, const LMS7002M_dir_t direction, const double fref, const double fout, double *factual)
+typedef struct
 {
-    LMS7_logf(LMS7_INFO, "SXX tune %f MHz (fref=%f MHz) begin", fout/1e6, fref/1e6);
+    int DIV_LOCH_SX;
+    double Ndiv;
+    int Nint;
+    int Nfrac;
+    double fvco;
+    int fdiv;
+    int EN_DIV2;
+    bool good;
+}
+LMS7002M_sxx_tune_state;
 
-    LMS7002M_set_mac_dir(self, direction);
-
+int LMS7002M_sxx_calc_tune_state(
+    const double fref, const double fout,
+    const double VCO_LO, const double VCO_HI,
+    LMS7002M_sxx_tune_state *s)
+{
     //The equations:
     // fref * N = fvco
     // fout = Fvco / divRatio_LOCH / 2
-    int DIV_LOCH_SX = -1;
+    s->DIV_LOCH_SX = -1;
     int divRatio_LOCH = 0;
-    double Ndiv = 0;
-    double fvco = 0;
-    int fdiv = 0;
+    s->Ndiv = 0;
+    s->fvco = 0;
+    s->fdiv = 0;
+    s->good = false;
 
     //calculation loop to find dividers that are possible
     while (true)
     {
         //try the next divider power
-        DIV_LOCH_SX++;
-        divRatio_LOCH = 1 << DIV_LOCH_SX;
-        fdiv = divRatio_LOCH*2;
+        s->DIV_LOCH_SX++;
+        divRatio_LOCH = 1 << s->DIV_LOCH_SX;
+        s->fdiv = divRatio_LOCH*2;
 
-        Ndiv = fout*fdiv/fref;
-        fvco = fout*fdiv;
-        LMS7_logf(LMS7_DEBUG, "fdiv = %d, Ndiv = %f, fvco = %f MHz", fdiv, Ndiv, fvco/1e6);
+        s->Ndiv = fout*s->fdiv/fref;
+        s->fvco = fout*s->fdiv;
+        LMS7_logf(LMS7_DEBUG, "fdiv = %d, Ndiv = %f, fvco = %f MHz", s->fdiv, s->Ndiv, s->fvco/1e6);
 
         //check dividers and vco in range...
-        if (fdiv > 128) return -1;
-        if (Ndiv < 4) continue;
-        if (Ndiv > 512) return -1;
+        if (s->fdiv > 128) return -1;
+        if (s->Ndiv < 4) continue;
+        if (s->Ndiv > 512) return -1;
 
         //check vco boundaries
-        if (fvco < LMS7002M_SXX_VCOL_LO) continue;
-        if (fvco > LMS7002M_SXX_VCOH_HI) continue;
+        if (s->fvco < VCO_LO) continue;
+        if (s->fvco > VCO_HI) continue;
 
         break; //its good
     }
-    LMS7_logf(LMS7_DEBUG, "fdiv = %d, Ndiv = %f, fvco = %f MHz", fdiv, Ndiv, fvco/1e6);
+    LMS7_logf(LMS7_DEBUG, "fdiv = %d, Ndiv = %f, fvco = %f MHz", s->fdiv, s->Ndiv, s->fvco/1e6);
 
     //deal with VCO divider
-    const int EN_DIV2 = (fvco > 5.5e9)?1:0;
+    s->EN_DIV2 = (s->fvco > 5.5e9)?1:0;
 
     //compensate for the lack of doubling when disabled
-    if (EN_DIV2 != 0) Ndiv /= 2;
+    if (s->EN_DIV2 != 0) s->Ndiv /= 2;
 
-    //after a successful tune, stash the frequency
-    if (direction == LMS_RX) self->sxr_freq = fout;
-    if (direction == LMS_TX) self->sxt_freq = fout;
-    if (direction == LMS_RX) self->sxr_fref = fref;
-    if (direction == LMS_TX) self->sxt_fref = fref;
+    //split N into fractional and integer parts
+    s->Nint = (int)s->Ndiv - 4;
+    s->Nfrac = (int)((s->Ndiv-((int)(s->Ndiv)))*(1 << 20));
 
-    //reset
-    self->regs->reg_0x011c_reset_n = 0;
-    LMS7002M_regs_spi_write(self, 0x011c);
-    self->regs->reg_0x011c_reset_n = 1;
-    LMS7002M_regs_spi_write(self, 0x011c);
+    s->good = true;
+    return 0;
+}
 
+void LMS7002M_sxx_apply_tune_state(LMS7002M_t *self, const LMS7002M_sxx_tune_state *s)
+{
     //configure and enable synthesizer
     self->regs->reg_0x011c_en_intonly_sdm = 0; //support frac-N
     self->regs->reg_0x011c_en_sdm_clk = 1; //enable
@@ -97,36 +108,55 @@ int LMS7002M_set_lo_freq(LMS7002M_t *self, const LMS7002M_dir_t direction, const
     self->regs->reg_0x011c_en_g = 1;
     self->regs->reg_0x011c_en_coarsepll = 0;
     self->regs->reg_0x0121_coarse_start = 0;
-    self->regs->reg_0x011c_en_div2_divprog = EN_DIV2;
+    self->regs->reg_0x011c_en_div2_divprog = s->EN_DIV2;
     self->regs->reg_0x011c_spdup_vco = 1; //fast settling
     LMS7002M_regs_spi_write(self, 0x011c);
 
     //program the N divider
-    const int Nint = (int)Ndiv - 4;
-    const int Nfrac = (int)((Ndiv-((int)(Ndiv)))*(1 << 20));
-    self->regs->reg_0x011d_frac_sdm = (Nfrac) & 0xffff; //lower 16 bits
-    self->regs->reg_0x011e_frac_sdm = (Nfrac) >> 16; //upper 4 bits
-    self->regs->reg_0x011e_int_sdm = Nint;
-    LMS7_logf(LMS7_DEBUG, "fdiv = %d, Ndiv = %f, Nint = %d, Nfrac = %d, DIV_LOCH_SX = %d, fvco = %f MHz", fdiv, Ndiv, Nint, Nfrac, DIV_LOCH_SX, fvco/1e6);
+    self->regs->reg_0x011d_frac_sdm = (s->Nfrac) & 0xffff; //lower 16 bits
+    self->regs->reg_0x011e_frac_sdm = (s->Nfrac) >> 16; //upper 4 bits
+    self->regs->reg_0x011e_int_sdm = s->Nint;
+    LMS7_logf(LMS7_DEBUG, "fdiv = %d, Ndiv = %f, Nint = %d, Nfrac = %d, DIV_LOCH_SX = %d, fvco = %f MHz", s->fdiv, s->Ndiv, s->Nint, s->Nfrac, s->DIV_LOCH_SX, s->fvco/1e6);
     LMS7002M_regs_spi_write(self, 0x011d);
     LMS7002M_regs_spi_write(self, 0x011e);
 
     //program the feedback divider
     self->regs->reg_0x011f_sel_sdmclk = REG_0X011F_SEL_SDMCLK_CLK_DIV;
-    self->regs->reg_0x011f_div_loch = DIV_LOCH_SX;
+    self->regs->reg_0x011f_div_loch = s->DIV_LOCH_SX;
     LMS7002M_regs_spi_write(self, 0x011f);
+}
+
+int LMS7002M_set_lo_freq(LMS7002M_t *self, const LMS7002M_dir_t direction, const double fref, const double fout, double *factual)
+{
+    LMS7_logf(LMS7_INFO, "SXX tune %f MHz (fref=%f MHz) begin", fout/1e6, fref/1e6);
+
+    LMS7002M_set_mac_dir(self, direction);
+
+    //reset
+    self->regs->reg_0x011c_reset_n = 0;
+    LMS7002M_regs_spi_write(self, 0x011c);
+    self->regs->reg_0x011c_reset_n = 1;
+    LMS7002M_regs_spi_write(self, 0x011c);
+
+    //state for each VCO
+    LMS7002M_sxx_tune_state states[3];
+    LMS7002M_sxx_calc_tune_state(fref, fout, LMS7002M_SXX_VCOL_LO, LMS7002M_SXX_VCOL_HI, states+0);
+    LMS7002M_sxx_calc_tune_state(fref, fout, LMS7002M_SXX_VCOM_LO, LMS7002M_SXX_VCOM_HI, states+1);
+    LMS7002M_sxx_calc_tune_state(fref, fout, LMS7002M_SXX_VCOH_LO, LMS7002M_SXX_VCOH_HI, states+2);
 
     //try several VCO settings to establish the best one
-    double VCO_LO[] = {LMS7002M_SXX_VCOL_LO, LMS7002M_SXX_VCOM_LO, LMS7002M_SXX_VCOH_LO};
-    double VCO_HI[] = {LMS7002M_SXX_VCOL_HI, LMS7002M_SXX_VCOM_HI, LMS7002M_SXX_VCOH_HI};
-
     int SEL_VCO_best = -1;
     int CSW_VCO_best = -1;
 
     for (int SEL_VCO_i = 0; SEL_VCO_i < 3; SEL_VCO_i++)
     {
-        if (fvco < VCO_LO[SEL_VCO_i]) continue;
-        if (fvco > VCO_HI[SEL_VCO_i]) continue;
+        LMS7002M_sxx_tune_state *s = states+SEL_VCO_i;
+
+        //filter out states without reasonable tune values
+        if (!s->good) continue;
+
+        //apply the configuration
+        LMS7002M_sxx_apply_tune_state(self, s);
 
         //select vco based on freq
         LMS7_logf(LMS7_DEBUG, "Testing for SEL_VCO = %d", SEL_VCO_i);
@@ -162,8 +192,18 @@ int LMS7002M_set_lo_freq(LMS7002M_t *self, const LMS7002M_dir_t direction, const
     self->regs->reg_0x011c_spdup_vco = 0; //done with fast settling
     LMS7002M_regs_spi_write(self, 0x011c);
 
+    //apply the best configuration
+    LMS7002M_sxx_tune_state *s = states+SEL_VCO_best;
+    LMS7002M_sxx_apply_tune_state(self, s);
+
+    //after a successful tune, stash the frequency
+    if (direction == LMS_RX) self->sxr_freq = fout;
+    if (direction == LMS_TX) self->sxt_freq = fout;
+    if (direction == LMS_RX) self->sxr_fref = fref;
+    if (direction == LMS_TX) self->sxt_fref = fref;
+
     //calculate the actual rate
-    if (factual != NULL) *factual = (1 << EN_DIV2) * fref * ((Nint+4) + (Nfrac/((double)(1 << 20)))) / fdiv;
+    if (factual != NULL) *factual = (1 << s->EN_DIV2) * fref * ((s->Nint+4) + (s->Nfrac/((double)(1 << 20)))) / s->fdiv;
 
     return 0; //OK
 }
